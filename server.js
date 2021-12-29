61aac736325639f5b3348bd871dab093d03f5bdb
kousic <kousic2000@gmail.com>
2021-12-29 19:25:47 +0530

    fixed error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv/config");
+require("dotenv").config({ path: ".env" });
 
 //To convert all to JSON format
 app.use(express.json());
