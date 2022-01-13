695726a77f34950158789b52e7b0fe0a460bbdb3
kousic <kousic2000@gmail.com>
2022-01-13 19:10:38 +0530

    fixed error

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
+const client = require("socket.io").listen(process.env.PORT || 5000).sockets;
 require("dotenv/config");
 
 //To convert all to JSON format
@@ -14,28 +15,18 @@ app.get("/", (req, res) => {
 });
 
 //DB connection
-// mongoose.connect(
-//   process.env.DB_connection,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   function (error) {
-//     if (error) {
-//       console.log("Database error or database connection error " + error);
-//     } else {
-//       console.log("Database state is " + mongoose.connection.readyState);
-//       console.log("DB connected");
-//     }
-//   }
-// );
-mongoose.connect(process.env.DB_connection, () => {
-  console.log("DB connection established");
-});
-// mongoose.connection
-//   .once("open", function () {
-//     console.log("Conection has been made!");
-//   })
-//   .on("error", function (error) {
-//     console.log("Error is: ", error);
-//   });
+mongoose.connect(
+  process.env.DB_connection,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (error) {
+    if (error) {
+      console.log("Database error or database connection error " + error);
+    } else {
+      console.log("Database state is " + mongoose.connection.readyState);
+      console.log("DB connected");
+    }
+  }
+);
 
 app.use("/uploads", express.static("uploads"));
 
@@ -66,6 +57,63 @@ app.use("/getAllDoc", getAllDoc_Route);
 
 const docAvatar_Route = require("./routes/docAvatar");
 app.use("/docAvatar", docAvatar_Route);
+
+//chat
+//Connect to Socket.io
+client.on("connection", function (socket) {
+  let chat = db.collection("chats");
+
+  //Create function to send status
+  sendStatus = function (s) {
+    socket.emit("status", s);
+  };
+
+  //Get chats from mongo collection
+  chat
+    .find()
+    .limit(100)
+    .sort({ _id: 1 })
+    .toArray(function (err, res) {
+      if (err) {
+        throw err;
+      }
+
+      //Emit the messages
+      socket.emit("output", res);
+    });
+
+  //Handle input events
+  socket.on("input", function (data) {
+    let name = data.name;
+    let message = data.message;
+
+    //Check for name and message
+    if (name == "" || message == "") {
+      //Send error status
+      sendStatus("Please enter a name and message");
+    } else {
+      //Insert message
+      chat.insert({ name: name, message: message }, function () {
+        client.emit("output", [data]);
+
+        //Send status object
+        sendStatus({
+          message: "Message sent",
+          clear: true,
+        });
+      });
+    }
+  });
+
+  //Handle clear
+  socket.on("clear", function (data) {
+    //Remove all chats from collection
+    chat.remove({}, function () {
+      //Emit cleared
+      socket.emit("cleared");
+    });
+  });
+});
 
 //to listen to the server
 app.listen(process.env.PORT || 5000);
